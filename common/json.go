7de00e706e6b6d512ce868fde132cdfa622fb648
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-02-05 17:07:41 +0100

    Added JSON type structure

---
+++ common/json.go
@@ -0,0 +1,36 @@
+/*
+ Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       Memento
+ Description   A backup system
+ License       GPL version 2 (see GPL.txt for details)
+*/
+
+package common
+
+import (
+	"encoding/json"
+	"net"
+)
+
+type JSONCommand struct {
+	Name string `json:name`
+}
+
+type JSONMessage struct {
+	Context string      `json:context`
+	Command JSONCommand `json:command`
+}
+
+func Sendresult(conn net.Conn, result, message string) {
+	type JSONResult struct {
+		Result  string `json:"result"`
+		Message string `json:"message"`
+	}
+
+	jsonmessage, _ := json.Marshal(&JSONResult{
+		Result:  result,
+		Message: message,
+	})
+
+	conn.Write(jsonmessage)
+}
