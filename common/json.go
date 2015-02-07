bc66f49829df4e9c65b5d374418d8a18c8382dfe
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-02-07 15:27:54 +0100

    Fixed typo

--- common/json.go
+++ common/json.go
@@ -45,7 +45,7 @@ type JSONResult struct {
 }
 
 func Sendresult(conn net.Conn, result JSONResult) {
-    jsonmessage, _ := json.Marshal(JSONResult)
+    jsonmessage, _ := json.Marshal(result)
 
 	conn.Write(jsonmessage)
 }
