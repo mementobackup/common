e8996d50d15df4a1e4fe28a7a53736778acaf637
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-02-09 22:44:47 +0100

    Added field in JSON file

--- common/json.go
+++ common/json.go
@@ -8,54 +8,55 @@
 package common
 
 import (
-    "encoding/json"
-    "net"
+	"encoding/json"
+	"net"
 )
 
 type JSONCommand struct {
-    Name      string `json:name`
-    Directory string `json:directory,omitempty`
-    ACL       bool   `json:acl,omitempty`
+	Name      string `json:name`
+	Directory string `json:directory,omitempty`
+	ACL       bool   `json:acl,omitempty`
 }
 
 type JSONMessage struct {
-    Context string      `json:context`
-    Command JSONCommand `json:command`
+	Context string      `json:context`
+	Command JSONCommand `json:command`
 }
 
 type JSONResult struct {
-    Result  string `json:"result"`
-    Message string `json:"message"`
+	Result  string `json:"result"`
+	Message string `json:"message"`
 }
 
 type JSONFileAcl struct {
-    Type  string `json:"type"`
-    Link  string `json:"link"`
-    Size  string `json:"size"`
-    Hash  string `json:"hash"`
-    Atime string `json:"atime,omitempty"`
-    Mtime string `json:"mtime,omitempty"`
-    Ctime string `json:"ctime,omitempty"`
-    Mode  string `json:"mode"`
-    User  string `json:"user"`
-    Group string `json:"group"`
+	Type  string `json:"type"`
+	Link  string `json:"link"`
+	Size  string `json:"size"`
+	Hash  string `json:"hash"`
+	Atime string `json:"atime,omitempty"`
+	Mtime string `json:"mtime,omitempty"`
+	Ctime string `json:"ctime,omitempty"`
+	Mode  string `json:"mode"`
+	User  string `json:"user"`
+	Group string `json:"group"`
 }
 
 type JSONFile struct {
-    Result string        `json:"result"`
-    Name   string        `json:"name"`
-    Os     string        `json:"os"`
-    Acl    []JSONFileAcl `json:"acl"`
+	Result string        `json:"result"`
+	Name   string        `json:"name"`
+	Type   string        `json:"type"`
+	Os     string        `json:"os"`
+	Acl    []JSONFileAcl `json:"acl"`
 }
 
 func (j *JSONResult) Send(conn net.Conn) {
-    jsonmessage, _ := json.Marshal(j)
+	jsonmessage, _ := json.Marshal(j)
 
-    conn.Write(jsonmessage)
+	conn.Write(jsonmessage)
 }
 
 func (j *JSONFile) Send(conn net.Conn) {
-    jsonmessage, _ := json.Marshal(j)
+	jsonmessage, _ := json.Marshal(j)
 
-    conn.Write(jsonmessage)
+	conn.Write(jsonmessage)
 }
