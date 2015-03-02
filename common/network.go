e03b09668786f36d737b0f7e3db52704fe5f3147
Enrico Bianchi <enrico.bianchi@gmail.com>
2015-03-02 15:27:33 +0100

    Added sending file function

---
+++ common/network.go
@@ -0,0 +1,34 @@
+/*
+ Copyright (C) 2015 Enrico Bianchi (enrico.bianchi@gmail.com)
+ Project       Memento
+ Description   A backup system
+ License       GPL version 2 (see GPL.txt for details)
+*/
+
+package common
+
+import (
+	"io"
+	"net"
+	"os"
+	"strings"
+)
+
+const BUFFER = 1024
+
+func Sendfile(filename string, connection net.Conn) {
+	var err error
+
+	file, err := os.Open(strings.TrimSpace(filename)) // For read access.
+	if err != nil {
+		connection.Write([]byte("-1"))
+		// TODO: manage error
+	}
+	defer file.Close()
+
+	_, err = io.Copy(connection, file)
+
+	if err != nil {
+		// TODO: manage error
+	}
+}
